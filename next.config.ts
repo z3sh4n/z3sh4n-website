9c4a92abf6edcc8bc3194d0ab7d2e15fa52ae924
Md. Zeeshan Ghaniwala <ghaniwalamohmmad@gmail.com>
2024-11-09 12:35:12 +0530

    almost everything is now in order

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'media1.tenor.com',
+      port: '',
+      pathname: '/**',
+    },
+  ],
+  },
 };
 
 export default nextConfig;
